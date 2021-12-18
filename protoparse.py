cd23dc73777fc3afaa9e4adcb5946f9212758fcc
Chirag Bapat <bapatchirag@gmail.com>
2021-12-18 20:07:20 +0530

    Added TCP Segment parsing

--- protoparse.py
+++ protoparse.py
@@ -4,12 +4,15 @@ import struct
 import sys
 import socket
 
+# Convert raw mac address to readable mac address
 def get_mac_addr(raw_addr):
     return raw_addr.hex(":")
 
+# Convert raw ip address to readable ip address
 def get_ip_addr(raw_addr):
     return ".".join(map(str, raw_addr))
 
+# Parse ethernet frame
 def ethernet_head(raw_data):
     dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
     dest_mac = get_mac_addr(dest)
@@ -19,6 +22,7 @@ def ethernet_head(raw_data):
     
     return dest_mac, src_mac, proto, data
 
+# Parse ipv4 packet
 def ipv4_head(eth_data):
     eth_open_byte = eth_data[0]
     ip_version = eth_open_byte >> 4
@@ -27,3 +31,19 @@ def ipv4_head(eth_data):
     tl_data = eth_data[header_len:]
     
     return ip_version, header_len, ttl, proto, get_ip_addr(src), get_ip_addr(target), tl_data
+
+# Parse tcp packet
+def tcp_head(ip_data):
+    src_port, dest_port, seq, ack, offset_res, flags = struct.unpack('! H H L L B B', ip_data[:14])
+    offset = (offset_res >> 4) * 4
+    flag_vals = {
+        "flag_URG": (flags & 32) >> 5,
+        "flag_ACK": (flags & 16) >> 4,
+        "flag_PSH": (flags & 8) >> 3,
+        "flag_RST": (flags & 4) >> 2,
+        "flag_SYN": (flags & 2) >> 1,
+        "flag_FIN": flags & 1
+    }
+    data = ip_data[offset:]
+    
+    return src_port, dest_port, seq, ack, flag_vals, data
--- sniffler.py
+++ sniffler.py
@@ -22,5 +22,16 @@ def main():
             print('\n\t - IPv4 Packet:')
             print('\t\t - IP Version: {}, Header Length: {}, TTL: {},'.format(ipv4[0], ipv4[1], ipv4[2]))
             print('\t\t - Protocol: {}, Source: {}, Target: {}'.format(ipv4[3], ipv4[4], ipv4[5]))
+            
+            # TCP Packet
+            if ipv4[3] == 6:
+                tcp = protoparse.tcp_head(ipv4[6])
+                print('\n\t\t - TCP Segment:')
+                print('\t\t\t - Source Port: {}, Destination Port: {}'.format(tcp[0], tcp[1]))
+                print('\t\t\t - Sequence Number: {}, Acknowledgement Number: {}'.format(tcp[2], tcp[3]))
+                print('\t\t\t - Flags:')
+                for flag in tcp[4]:
+                    print('\t\t\t\t - {}: {}'.format(flag[5:], tcp[4][flag]))
+                print('\t\t\t - Data: {}'.format(tcp[5]))
         
 main()
