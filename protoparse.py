2af418b3d30e38e52b17658ad810508431b0623e
Chirag Bapat <bapatchirag@gmail.com>
2021-12-18 09:32:03 +0530

    Added IPv4 header parsing

--- protoparse.py
+++ protoparse.py
@@ -7,10 +7,23 @@ import socket
 def get_mac_addr(raw_addr):
     return raw_addr.hex(":")
 
+def get_ip_addr(raw_addr):
+    return ".".join(map(str, raw_addr))
+
 def ethernet_head(raw_data):
     dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
     dest_mac = get_mac_addr(dest)
     src_mac = get_mac_addr(src)
     proto = socket.htons(prototype)
     data = raw_data[14:]
+    
     return dest_mac, src_mac, proto, data
+
+def ipv4_head(eth_data):
+    eth_open_byte = eth_data[0]
+    ip_version = eth_open_byte >> 4
+    header_len = (eth_open_byte & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', eth_data[:20])
+    tl_data = eth_data[header_len:]
+    
+    return ip_version, header_len, ttl, proto, get_ip_addr(src), get_ip_addr(target), tl_data
--- sniffler.py
+++ sniffler.py
@@ -10,8 +10,17 @@ def main():
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     while True:
         raw_data, addr = s.recvfrom(65535)
+        
+        # Ethernet Frame
         eth = protoparse.ethernet_head(raw_data)
         print('\nEthernet Frame:')
         print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1],eth[2]))
+        
+        # IP Packet
+        if eth[2] == 8:
+            ipv4 = protoparse.ipv4_head(eth[3])
+            print('\n\t - IPv4 Packet:')
+            print('\t\t - IP Version: {}, Header Length: {}, TTL: {},'.format(ipv4[0], ipv4[1], ipv4[2]))
+            print('\t\t - Protocol: {}, Source: {}, Target: {}'.format(ipv4[3], ipv4[4], ipv4[5]))
         
 main()
