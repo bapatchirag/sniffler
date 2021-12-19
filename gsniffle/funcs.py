c3895354cb280996b54c9d2432d9871656c1ccf3
Chirag Bapat <bapatchirag@gmail.com>
2021-12-19 23:38:10 +0530

    Added button test functionality

--- gsniffle/funcs.py
+++ gsniffle/funcs.py
@@ -1,4 +1,28 @@
 # Add functionalities
 
+from PyQt5.QtWidgets import QMessageBox
+
+# Testing alert box
+def alert_box(title="Test", text="Button works!"):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Information)
+    msg.setText(text)
+    msg.setWindowTitle(title)
+    msg.setStandardButtons(QMessageBox.Ok)
+    msg.exec_()
+
+# Callback to apply filters
 def apply_filters():
-    pass
+    alert_box()
+
+# Callback to start sniffing packets
+def start_sniffing():
+    alert_box()
+
+# Callback to stop sniffing packets
+def stop_sniffing():
+    alert_box()
+
+# Callback to open analysis window
+def do_analysis():
+    alert_box()
