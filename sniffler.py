3fdbef37d424da8b979cb8770c300697491d42ce
Chirag Bapat <bapatchirag@gmail.com>
2021-12-19 09:29:08 +0530

    Cleaned up filtering for packets/segments by protocol

--- sniffler.py
+++ sniffler.py
@@ -14,6 +14,26 @@ filter_to_protocol = {
 def filters():
     return input("Enter filters: ").split(" ")
 
+def display_ipv4_packet(ip_data):
+    print('\n\t - IPv4 Packet:')
+    print('\t\t - IP Version: {}, Header Length: {}, TTL: {},'.format(ip_data[0], ip_data[1], ip_data[2]))
+    print('\t\t - Protocol: {}, Source: {}, Target: {}'.format(ip_data[3], ip_data[4], ip_data[5]))
+
+def display_tcp_packet(tcp_data):
+    print('\n\t - TCP Packet:')
+    print('\t\t - Source Port: {}, Destination Port: {}'.format(tcp_data[0], tcp_data[1]))
+    print('\t\t - Sequence Number: {}, Acknowledgement Number: {}'.format(tcp_data[2], tcp_data[3]))
+    print('\t\t - Flags:')
+    for flag in tcp_data[4]:
+        print('\t\t\t - {}: {}'.format(flag[5:], tcp_data[4][flag]))
+    print('\t\t - Data: {}'.format(tcp_data[5]))
+    
+def display_udp_packet(udp_data):
+    print('\n\t\t - UDP Segment:')
+    print('\t\t\t - Source Port: {}, Destination Port: {}'.format(udp_data[0], udp_data[1]))
+    print('\t\t\t - Data: {}'.format(udp_data[2]))
+    
+
 # receive a packet
 def main():
     filter_names = filters()
@@ -23,37 +43,27 @@ def main():
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     while True:
         raw_data, addr = s.recvfrom(65535)
-        eth = [0 for x in range(14)]
         
-        # Ethernet Frame
-        if 0 in protocols:
-            eth = protoparse.ethernet_head(raw_data)
+        # Ethernet frame
+        eth = protoparse.ethernet_head(raw_data)
+        if 0 in protocols:            
             print('\nEthernet Frame:')
             print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1],eth[2]))
         
         # IP Packet
         if eth[2] == 8:
             ipv4 = protoparse.ipv4_head(eth[3])
-            print('\n\t - IPv4 Packet:')
-            print('\t\t - IP Version: {}, Header Length: {}, TTL: {},'.format(ipv4[0], ipv4[1], ipv4[2]))
-            print('\t\t - Protocol: {}, Source: {}, Target: {}'.format(ipv4[3], ipv4[4], ipv4[5]))
             
             # TCP Packet
             if ipv4[3] == 6 and ipv4[3] in protocols:
+                display_ipv4_packet(ip_data=ipv4)
                 tcp = protoparse.tcp_head(ipv4[6])
-                print('\n\t\t - TCP Segment:')
-                print('\t\t\t - Source Port: {}, Destination Port: {}'.format(tcp[0], tcp[1]))
-                print('\t\t\t - Sequence Number: {}, Acknowledgement Number: {}'.format(tcp[2], tcp[3]))
-                print('\t\t\t - Flags:')
-                for flag in tcp[4]:
-                    print('\t\t\t\t - {}: {}'.format(flag[5:], tcp[4][flag]))
-                print('\t\t\t - Data: {}'.format(str(tcp[5])))
+                display_tcp_packet(tcp_data=tcp)
                 
             # UDP Packet
             elif ipv4[3] == 17 and ipv4[3] in protocols:
+                display_ipv4_packet(ip_data=ipv4)
                 udp = protoparse.udp_head(ipv4[6])
-                print('\n\t\t - UDP Segment:')
-                print('\t\t\t - Source Port: {}, Destination Port: {}'.format(udp[0], udp[1]))
-                print('\t\t\t - Data: {}'.format(udp[2]))
+                display_udp_packet(udp_data=udp)
         
 main()
